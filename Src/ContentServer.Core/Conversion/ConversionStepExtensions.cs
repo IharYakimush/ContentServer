55ce4a1d48f72e9824903574e125962eee635f21
IharYakimush <Ihar_Yakimush@outlook.com>
2022-04-24 22:09:58 +0300

    parse regex

--- Src/ContentServer.Core/Conversion/ConversionStepExtensions.cs
+++ Src/ContentServer.Core/Conversion/ConversionStepExtensions.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ContentServer.Core.Conversion
 {
     public static class ConversionStepExtensions
     {
+        private static readonly Regex funcReg = new Regex(@"^([a-z]*)\((.*)\)$");
+
+        private static readonly Regex argReg = new Regex(@"(?:[^\)\(,]+|\([^\)\(]+\))+");
         public static IReadOnlyCollection<ConversionStep> Parse(string value)
         {
             return new ConversionStep[0];

e62fb28c9f085e868b23fe896e749b41b8ad37c0
IharYakimush <Ihar_Yakimush@outlook.com>
2022-04-24 21:01:12 +0300

    parser tests

---
+++ Tests/ContentServer.Tests/Conversion/ConversionStepTests.cs
@@ -0,0 +1,28 @@
+﻿using ContentServer.Core.Conversion;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace ContentServer.Tests.Conversion
+{
+    public class ConversionStepTests
+    {
+        [Theory]// Chnage result id to be equal etag ??
+        [InlineData("")]//jpg to png
+        public void Parse(string resourceName)
+        {
+            var steps = ConversionStepExtensions.Parse(resourceName);
+
+            var result = JsonSerializer.Serialize(steps);
+            using StreamReader reader = new StreamReader(typeof(ConversionStepTests).Assembly.GetManifestResourceStream("ContentServer.Tests.Conversion.ParseConversionStep." + resourceName + ".json"));
+            Assert.Equal(reader.ReadToEnd(), result);
+        }
+    }
+}

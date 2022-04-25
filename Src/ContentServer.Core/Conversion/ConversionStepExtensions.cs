7dcb8e5a508f07de419e49d7eca3338d1205a02b
IharYakimushEpam <Ihar_Yakimush@epam.com>
2022-04-25 21:18:58 +0300

    parse url

--- Src/ContentServer.Core/Conversion/ConversionStepExtensions.cs
+++ Src/ContentServer.Core/Conversion/ConversionStepExtensions.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace ContentServer.Core.Conversion
 {
@@ -12,29 +7,49 @@ namespace ContentServer.Core.Conversion
         private static readonly Regex funcReg = new Regex(@"^([a-z]*)\((.*)\)$");
 
         private static readonly Regex argReg = new Regex(@"(?:[^\)\(,]+|\([^\)\(]+\))+");
-        public static IReadOnlyCollection<ConversionStep> Parse(string value)
-        {
-
-            return Array.Empty<ConversionStep>();
-        }
-
-        private static ConversionStep ParseStep(string value)
+        public static IEnumerable<ConversionStep> ParseSteps(string value)
         {
             string fname = "default";
             var fmatch = funcReg.Match(value);
             if (fmatch.Success)
             {
-                fname = fmatch.Groups[0].Value ?? fname;
-                value = fmatch.Groups[1].Value;
+                fname = string.IsNullOrEmpty(fmatch.Groups[1].Value) ? fname : fmatch.Groups[1].Value;
+                value = fmatch.Groups[2].Value;
             }
 
             var pmatch = argReg.Match(value);
             if (!pmatch.Success)
             {
-                throw new ArgumentException("params", nameof(value));
+                throw new ArgumentException($"Unable to parse params {value}", nameof(value));
             }
 
-            throw new NotImplementedException();
+            var pv = pmatch.Groups.Values.Select(g => g.Value);
+
+            List<string> input = new();
+            List<KeyValuePair<string, string>> param = new();
+
+            foreach (var v in pv)
+            {
+                if (v.StartsWith("$", StringComparison.Ordinal))
+                {
+                    input.Add(v.TrimStart('$'));
+                }
+                else if (v.IndexOf('_', StringComparison.Ordinal) > 0)
+                {
+                    var s = v.Split('_', StringSplitOptions.RemoveEmptyEntries);
+                    param.Add(new KeyValuePair<string, string>(s[0], string.Join('_', s.Skip(1))));
+                }
+                else
+                {
+                    throw new ArgumentException($"Unable to parse param {v} in {value} for {fname} step", nameof(value));
+                }
+            }
+
+            ConversionDefinition conversion = new ConversionDefinition(fname, param.ToDictionary(kv => kv.Key, kv => kv.Value));
+
+            ConversionStep result = new ConversionStep(conversion, "result", input);
+
+            yield return result;
         }
     }
 }
--- Tests/ContentServer.Tests/Conversion/ConversionStepTests.cs
+++ Tests/ContentServer.Tests/Conversion/ConversionStepTests.cs
@@ -6,23 +6,43 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 using Xunit;
+using Xunit.Abstractions;
 
 namespace ContentServer.Tests.Conversion
 {
     public class ConversionStepTests
     {
-        [Theory]// Chnage result id to be equal etag ??
-        [InlineData("")]//jpg to png
+        public ConversionStepTests(ITestOutputHelper output)
+        {
+            Output = output ?? throw new ArgumentNullException(nameof(output));
+        }
+
+        public ITestOutputHelper Output { get; }
+
+        [Theory]
+        [InlineData("f_jpg")]
+        [InlineData("(f_jpg)")]
+        [InlineData("f_jpg,w_100")]
+        [InlineData("qwe(f_jpg)")]
+        [InlineData("qwe(f_jpg,w_100)")]
         public void Parse(string resourceName)
         {
-            var steps = ConversionStepExtensions.Parse(resourceName);
+            this.Output.WriteLine(resourceName);
+            var steps = ConversionStepExtensions.ParseSteps(resourceName).ToArray();
 
-            var result = JsonSerializer.Serialize(steps);
+            JsonSerializerOptions options = new JsonSerializerOptions();
+            options.WriteIndented = true;
+
+            var result = JsonSerializer.Serialize(steps, options);
+            this.Output.WriteLine(result);
             using StreamReader reader = new StreamReader(typeof(ConversionStepTests).Assembly.GetManifestResourceStream("ContentServer.Tests.Conversion.ParseConversionStep." + resourceName + ".json"));
-            Assert.Equal(reader.ReadToEnd(), result);
+            Assert.Equal(
+                Regex.Replace(reader.ReadToEnd(), "(\"(?:[^\"\\\\]|\\\\.)*\")|\\s+", "$1"), 
+                Regex.Replace(result, "(\"(?:[^\"\\\\]|\\\\.)*\")|\\s+", "$1"));
         }
     }
 }

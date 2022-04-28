09fb8fa1de3c65f9c8b6dc965d29589dbaaf003f
Ihar Yakimush <Ihar_Yakimush@epam.com>
2022-04-28 15:52:49 +0300

    api gen

---
+++ Tests/ConversionServer.Core.Tests/PublicApiTests.cs
@@ -0,0 +1,18 @@
+﻿using PublicApiGenerator;
+using Xunit;
+
+namespace ConversionServer.Core.Tests;
+
+public class PublicApiTests
+{
+    [Fact]
+    public void Test()
+    {
+        string publicApi = typeof(ConversionAction).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+        {
+            IncludeAssemblyAttributes = false,
+        });
+
+        // TODO: verify
+    }
+}
